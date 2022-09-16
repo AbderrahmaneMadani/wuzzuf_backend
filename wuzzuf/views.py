9c9efd7e0fbed521fa67d801cee6644eb674f17e
abde0344 <abde0344@gmail.com>
2022-09-16 16:33:18 +0100

    -create index.html like example for test -relate functions in view.py with urls.py -include urls of wuzzuf in urls of wuzzuf_backend project

--- wuzzuf/views.py
+++ wuzzuf/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def index(request):
+
+    return HttpResponse('hello world')
+
+
+
+
+def index1(request):
+    x = {'name': 'ahmed', 'age': 25}
+    return render(request, 'pages/index.html',x)
